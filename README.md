4f15854721ee700baa35cb3e990dc7154b34e377
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2024-07-22 20:09:14 +0900

    feat: 最終発表

---
+++ README.md
@@ -0,0 +1,3 @@
+## IoT特論最終課題
+
+https://gomadoufu.notion.site/68d777869e764e36914a940a21c4393f
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@ use std::mem;
 use embassy_futures::select::{select, Either};
 
 use esp_idf_svc::eventloop::EspSystemEventLoop;
+use esp_idf_svc::hal::delay::Delay;
+use esp_idf_svc::hal::i2c::{I2cConfig, I2cDriver};
+use esp_idf_svc::hal::modem::Modem;
 use esp_idf_svc::hal::peripherals::Peripherals;
+use esp_idf_svc::hal::prelude::*;
 use esp_idf_svc::mqtt::client::*;
 use esp_idf_svc::nvs::EspDefaultNvsPartition;
 use esp_idf_svc::sys::EspError;
@@ -14,7 +18,16 @@ use esp_idf_svc::timer::{EspAsyncTimer, EspTaskTimerService, EspTimerService};
 use esp_idf_svc::tls::X509;
 use esp_idf_svc::wifi::*;
 
+use esp_idf_svc::hal::{
+    gpio::{InterruptType, PinDriver, Pull},
+    task::notification::Notification,
+};
+use mpu6886::Mpu6886;
+use std::num::NonZeroU32;
+
 use log::*;
+
+use anyhow::Result;
 
 #[toml_cfg::toml_config]
 pub struct Config {
@@ -30,35 +43,73 @@ pub struct Config {
     aws_iot_topic: &'static str,
 }
 
-fn convert_certificate(mut certificate_bytes: Vec<u8>) -> X509<'static> {
-    // append NUL
-    certificate_bytes.push(0);
-
-    // convert the certificate
-    let certificate_slice: &[u8] = unsafe {
-        let ptr: *const u8 = certificate_bytes.as_ptr();
-        let len: usize = certificate_bytes.len();
-        mem::forget(certificate_bytes);
-
-        slice::from_raw_parts(ptr, len)
-    };
-
-    // return the certificate file in the correct format
-    X509::pem_until_nul(certificate_slice)
-}
-
 fn main() {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
+    let peripherals = Peripherals::take().unwrap();
+
+    // Configures the button
+    let mut button = PinDriver::input(peripherals.pins.gpio42).unwrap();
+    button.set_pull(Pull::Up).unwrap();
+    button.set_interrupt_type(InterruptType::PosEdge).unwrap();
+
+    // Configures the notification
+    let notification = Notification::new();
+    let notifier = notification.notifier();
+
+    // Safety: make sure the `Notification` object is not dropped while the subscription is active
+    unsafe {
+        button
+            .subscribe(move || {
+                notifier.notify_and_yield(NonZeroU32::new(1).unwrap());
+            })
+            .unwrap();
+    }
+    info!("sensor initialized");
+
+    // 1. Instanciate the SDA and SCL pins, correct pins are in the training material.
+    let sda = peripherals.pins.gpio13;
+    let scl = peripherals.pins.gpio15;
+    // 2. Instanciate the i2c peripheral
+    let config = I2cConfig::new().baudrate(400.kHz().into());
+    let i2c = I2cDriver::new(peripherals.i2c0, sda, scl, &config).unwrap();
+    info!("I2C initialized");
+
+    let mut delay = Delay::default();
+    let mut mpu = Mpu6886::new(i2c);
+
+    mpu.init(&mut delay).unwrap();
+    info!("sensor initialized");
+
     let app_config = CONFIG;
+    info!("WIFI SSID = {}", app_config.wifi_ssid);
+    info!("WIFI PASS = {}", app_config.wifi_password);
+    info!("AWS IoT Endpoint = {}", app_config.aws_iot_endpoint);
+    info!("AWS IoT Client ID = {}", app_config.aws_iot_client_id);
+    info!("AWS IoT Topic = {}", app_config.aws_iot_topic);
 
     let sys_loop = EspSystemEventLoop::take().unwrap();
     let timer_service = EspTimerService::new().unwrap();
     let nvs = EspDefaultNvsPartition::take().unwrap();
 
+    info!("ESP IDF SVC initialized");
+
+    let mut buzzer = PinDriver::output(peripherals.pins.gpio2).unwrap();
+    buzzer.set_low().unwrap();
+    buzzer.set_high().unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(3));
+    buzzer.set_low().unwrap();
+
     esp_idf_svc::hal::task::block_on(async {
-        let _wifi = wifi_create(&app_config, &sys_loop, &timer_service, &nvs).await?;
+        let _wifi = wifi_create(
+            peripherals.modem,
+            &app_config,
+            &sys_loop,
+            &timer_service,
+            &nvs,
+        )
+        .await?;
         info!("Wifi created");
 
         let server_cert =
@@ -79,12 +130,20 @@ fn main() {
         info!("MQTT client created");
 
         let mut timer = timer_service.timer_async()?;
-        run(&mut client, &mut conn, &mut timer, app_config.aws_iot_topic).await
+        run(
+            &mut mpu,
+            &mut client,
+            &mut conn,
+            &mut timer,
+            app_config.aws_iot_topic,
+        )
+        .await
     })
     .unwrap();
 }
 
 async fn run(
+    mpu: &mut Mpu6886<I2cDriver<'_>>,
     client: &mut EspAsyncMqttClient,
     connection: &mut EspAsyncMqttConnection,
     timer: &mut EspAsyncTimer,
@@ -128,9 +187,19 @@ async fn run(
                 // Just to give a chance of our connection to get even the first published message
                 timer.after(Duration::from_millis(500)).await?;
 
-                let payload = "Hello from esp-mqtt-demo!";
-
+                //main loop
                 loop {
+                    // get gyro data, scaled with sensitivity
+                    let gyro = mpu.get_gyro().unwrap();
+                    println!("gyro: {:?}", gyro);
+
+                    // get accelerometer data, scaled with sensitivity
+                    let acc = mpu.get_acc().unwrap();
+                    println!("acc: {:?}", acc);
+                    std::thread::sleep(std::time::Duration::from_secs(1));
+
+                    let payload = format!("{{\"gyro\": {:?}, \"acc\": {:?}}}", gyro, acc);
+
                     client
                         .publish(topic, QoS::AtMostOnce, false, payload.as_bytes())
                         .await?;
@@ -176,14 +245,13 @@ fn mqtt_create(
 }
 
 async fn wifi_create(
+    modem: Modem,
     app_config: &Config,
     sys_loop: &EspSystemEventLoop,
     timer_service: &EspTaskTimerService,
     nvs: &EspDefaultNvsPartition,
 ) -> Result<EspWifi<'static>, EspError> {
-    let peripherals = Peripherals::take()?;
-
-    let mut esp_wifi = EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs.clone()))?;
+    let mut esp_wifi = EspWifi::new(modem, sys_loop.clone(), Some(nvs.clone()))?;
     let mut wifi = AsyncWifi::wrap(&mut esp_wifi, sys_loop.clone(), timer_service.clone())?;
 
     wifi.set_configuration(&Configuration::Client(ClientConfiguration {
@@ -202,4 +270,21 @@ async fn wifi_create(
     info!("Wifi netif up");
 
     Ok(esp_wifi)
+}
+
+fn convert_certificate(mut certificate_bytes: Vec<u8>) -> X509<'static> {
+    // append NUL
+    certificate_bytes.push(0);
+
+    // convert the certificate
+    let certificate_slice: &[u8] = unsafe {
+        let ptr: *const u8 = certificate_bytes.as_ptr();
+        let len: usize = certificate_bytes.len();
+        mem::forget(certificate_bytes);
+
+        slice::from_raw_parts(ptr, len)
+    };
+
+    // return the certificate file in the correct format
+    X509::pem_until_nul(certificate_slice)
 }
